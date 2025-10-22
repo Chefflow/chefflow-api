dc34d9bafd936280fe8ee6ce097f04e4e989aa3e
Imcasero <diegocaserosmr@gmail.com>
2025-10-22 21:22:42 +0200

    refactor: remove AppService and related tests

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,18 +1,9 @@
 import { Controller, Get, HttpException, HttpStatus } from '@nestjs/common';
-import { AppService } from './app.service';
 import { PrismaService } from './prisma.service';
 
 @Controller()
 export class AppController {
-  constructor(
-    private readonly appService: AppService,
-    private readonly prisma: PrismaService,
-  ) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+  constructor(private readonly prisma: PrismaService) {}
 
   @Get('health')
   getHealth(): { status: string; timestamp: string } {
