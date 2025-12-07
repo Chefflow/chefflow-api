9f5f56a688c16f1706710a884836c1f9f76da21e
Imcasero <diegocaserosmr@gmail.com>
2025-12-07 22:46:35 +0100

    chore: update application port to 3000 and bind to all network interfaces

--- src/main.ts
+++ src/main.ts
@@ -38,8 +38,8 @@ async function bootstrap() {
     }),
   );
 
-  const port = process.env.PORT ?? 4000;
-  await app.listen(port);
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port, '0.0.0.0');
 
   console.log(`Application is running on: http://localhost:${port}`);
 }
