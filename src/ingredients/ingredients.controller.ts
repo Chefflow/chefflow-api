957480f57fb21d72eb50e66ae672babd50c1bed7
Imcasero <diegocaserosmr@gmail.com>
2025-12-16 21:53:58 +0100

    feat: implement CRUD operations in IngredientsController for ingredient management

--- src/ingredients/ingredients.controller.ts
+++ src/ingredients/ingredients.controller.ts
@@ -1,7 +1,53 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { IngredientsService } from './ingredients.service';
+import { CreateIngredientDto } from './dto/create-ingredient.dto';
+import { UpdateIngredientDto } from './dto/update-ingredient.dto';
+import { RecipeIngredient } from '@prisma/client';
 
 @Controller('ingredients')
 export class IngredientsController {
   constructor(private readonly ingredientsService: IngredientsService) {}
+
+  @Post()
+  @HttpCode(HttpStatus.OK)
+  async create(@Body() createIngredientDto: CreateIngredientDto) {
+    return `Ingredinet ${CreateIngredientDto.name} was created`;
+  }
+
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  async find(@Param('search') ingredient: string) {
+    return `Ingredient ${ingredient} return`;
+  }
+
+  @Get(':id')
+  @HttpCode(HttpStatus.OK)
+  async findById(@Param('id') id: string) {
+    return `Ingredient with ${id} return`;
+  }
+
+  @Patch(':id')
+  @HttpCode(HttpStatus.OK)
+  async update(
+    @Param('id') id: string,
+    @Body() updateIngredient: UpdateIngredientDto,
+  ) {
+    return `For the ${id} updated to ${updateIngredient}`;
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.OK)
+  async delete(@Param(':id') id: string) {
+    return `delete ingredient with the id: ${id}`;
+  }
 }
